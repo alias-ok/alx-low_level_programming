6b63d129faa6f48c4910d3819a05953a779a5cc2
alias-ok <okthese@outlook.com>
2023-02-17 22:21:38 +0000

    "Print alphabet in lowercase"

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*standard input output as the header*/
+/*main - basically entry point*/
+/*return: always 0 (success)*/
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+putchar(c);
+return (0);
+}
