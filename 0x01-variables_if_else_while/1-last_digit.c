a08bf7cfd27590e04d48c191e2e388b2452d6757
alias-ok <okthese@outlook.com>
2023-02-17 21:53:31 +0000

    "I play what I call the alphabet game"

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,9 +13,9 @@ n = rand() - RAND_MAX / 2;
 m = n % 10;
 if (m > 5)
 printf("Last digit of %d is %d and is greater that 5\n", n, m);
-else if (m == 0)
-printf("The last digit of %d is %d and is 0\n", n, m);
-else
-printf("The last digit of %d is %d and is less than 6 and not 0\n", n, m);
+if (m == 0)
+printf("Last digit of %d is %d and is 0\n", n, m);
+if (m < 6 && m != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 return (0);
 }
