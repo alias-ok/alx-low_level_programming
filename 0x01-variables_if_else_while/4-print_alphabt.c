5da0e2452883fd6c305e4ab44a5aa96c7f6aff0d
alias-ok <okthese@outlook.com>
2023-02-17 23:14:09 +0000

    "4 print alphabets in lowercase"

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * return: value 0 for success
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+if ((c != 'q' && c != 'e') && c <= 'z')
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
