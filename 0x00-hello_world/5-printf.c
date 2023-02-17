dd6ddb0969c3e4b2e0c00a9621e6285a6c9808c4
alias-ok <okthese@outlook.com>
2023-02-17 00:28:17 +0000

    "5 printf dot c"

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - c program that displays a text using printf function
+*return : 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
