18a13b274b484f8c1310c55348a6f9daa29f488a
alias-ok <okthese@outlook.com>
2023-02-17 13:14:24 +0000

    "101 quote dot c"

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,7 @@
  * main - entry point
  * return: value 1
  */
+/* betty style doc for function main goes there */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
