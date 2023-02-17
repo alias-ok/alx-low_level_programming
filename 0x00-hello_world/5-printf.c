6c351ce9a5ca24b82c1fdc711a957526b3f8b239
alias-ok <okthese@outlook.com>
2023-02-17 00:53:18 +0000

    "5 printf dot c"

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* main - c program that displays a text using printf function
-*return : 0 (success when program returns 0)
+* main - Entry point
+* Description: C program that displays a text using printf function
+*return : This returns 0
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n"); /*inline*/
 return (0);
 }
