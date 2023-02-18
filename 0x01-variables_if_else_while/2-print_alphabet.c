606dcc0d880e4fd997132ba411faf263fc12e952
alias-ok <okthese@outlook.com>
2023-02-18 10:58:07 +0000

    "alphABET"

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/*standard input output as the header*/
-/*main - basically entry point*/
-/*return: always 0 (success)*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 for success
+ */
 int main(void)
 {
 char c;
