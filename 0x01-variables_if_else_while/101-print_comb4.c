7ced85c41c9b4f20f2693464287cd70f6c86bbb8
alias-ok <okthese@outlook.com>
2023-02-18 11:48:27 +0000

    "The success combination in business is: Do what you do better... and: do more of what you do..."

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  *
@@ -8,15 +9,14 @@ int main(void)
 {
 int c;
 int d;
-int e;
-e = 0;
+int e = 0;
 while (e < 10)
 {
 d = 0;
 while (d < 10)
 {
 c = 0;
-while (c < 0)
+while (c < 10)
 {
 if (c != d && d != e && e < d && d < c)
 {
