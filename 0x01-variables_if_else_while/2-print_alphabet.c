9acbb35f651bdb561037be152cd0bcda5c3bfd25
alias-ok <okthese@outlook.com>
2023-02-18 11:14:10 +0000

    "I play what I call the alphabet game"

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  *
@@ -6,9 +7,12 @@
  */
 int main(void)
 {
-char c;
-c = 'a';
+char c = 'a';
 while (c <= 'z')
+{
 putchar(c);
+c++;
+}
+putchar('\n');
 return (0);
 }
