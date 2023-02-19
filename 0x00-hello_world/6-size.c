bdafa0d970f019e7e52cfe2ca1b3e8ddc5028b43
alias-ok <okthese@outlook.com>
2023-02-19 22:45:44 +0000

    "size 32 and 64"

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ long long int d;
 float f;
 printf("size of a char: %i byte(s)\n", sizeof(char));
 printf("Size of an int: %i byte(s)\n", sizeof(int));
-printf("Size of a long: %i byte(s)\n", sizeof(long int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 printf("Size of a float: %i byte(s)\n", sizeof(float));
 return (0);
