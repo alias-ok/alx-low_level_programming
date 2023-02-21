92e3dc61172b5ad93d82a663f511e8e519c0c7dd
alias-ok <okthese@outlook.com>
2023-02-21 23:37:24 +0000

    "A function that computes the absolute value of an integer"

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Entry point that prints the absolute value of an integer
+ * @a: Parameter, var name
+ * Return: Always 0 for success
+ */
+int _abs(int a)
+{
+if (a < 0)
+{
+a = -a;
+}
+return (a);
+}
