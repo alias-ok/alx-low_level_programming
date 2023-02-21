0e7dfd477d530c9b5c496e84fd0743ae39de276f
alias-ok <okthese@outlook.com>
2023-02-21 14:51:16 +0000

    "Add 1-alphabet.c"

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point, prints the alphabet in lowercase
+ *
+ * Return: Always 0 for success
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{ 
+ _putchar(c);
+}
+ _putchar('\n');
+}
