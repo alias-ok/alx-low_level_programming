b9f35fcae420aae03e255714d3a93a1dbb9771aa
alias-ok <okthese@outlook.com>
2023-02-21 16:43:21 +0000

    "Add 3-islower.c"

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - Entry point that checks for lowercase character
+ *
+ * Return: return 1 if c lowercase and 0 when otherwise
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <=122)
+{
+return 1;
+}
+return 0;
+}
