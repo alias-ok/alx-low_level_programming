d3fdab7ec6284db0925bee595f5bf414ece2d558
alias-ok <okthese@outlook.com>
2023-02-22 02:17:49 +0000

    "10-add.c"

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Entry point that adds two numbers and displays result
+ * @n1: First parameter
+ * @n2: Second parameter
+ * Return: Always n1 + n2 for success
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
