03c8b36573b70156e02dd02734ca94817d5ce27a
alias-ok <okthese@outlook.com>
2023-02-22 01:16:39 +0000

    "A function that prints the last digit of a number"

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point that prints the last digit of a number
+ * @a: Parameter set
+ * Return 0 for success
+ */
+int print_last_digit(int a)
+{
+int last;
+last = a % 10;
+if (last < 0)
+{
+last = last * -1;
+}
+_putchar(last + 0);
+return (last);
+}
