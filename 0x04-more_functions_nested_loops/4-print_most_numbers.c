c0943f3aaaf96eed30f065dcea51d23d8edd7c45
alias-ok <okthese@outlook.com>
2023-02-23 16:35:24 +0000

    "I believe in numbers and signs, a function that prints the numbers, from 0 to 9"

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_most_numbers - Entry point that prints the numbers from 0 to 9
- * @d: Set parameter to store value
+ *
  * Return: ...
  */
 void print_most_numbers(void)
 {
 	int d = 0;
+
 	for (; d <= 9; d++)
 	{
 		if (d == 2 && d == 4)
@@ -15,9 +17,8 @@ void print_most_numbers(void)
 		}
 		else
 		{
-			_putchar(d + '0');
+			putchar(d + '0');
 		}
-		
 	}
-	_putchar('\n');
+	putchar('\n');
 }
