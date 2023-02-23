376968f5a6a07fab33bac47e775dafade881c2bb
alias-ok <okthese@outlook.com>
2023-02-23 14:31:27 +0000

    "Collaboration is multiplication, a function that multiplies two integers"

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Entry point that multiplies two integers
+ * @a: Set parameters to store value
+ * @b: Set parameters to store value
+ * Return: Always a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
