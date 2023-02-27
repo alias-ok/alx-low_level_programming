6995fd9a36a26b19a157bdca269f911778a0de31
alias-ok <okthese@outlook.com>
2023-02-27 22:58:51 +0000

    "Great leaders are willing to sacrifice the numbers to save the people"

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,29 +6,29 @@
  */
 int _atoi(char *s)
 {
-	int m = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+int m = 0;
+unsigned int ni = 0;
+int min = 1;
+int isi = 0;
 
-	while (s[m])
-	{
-		if (s[m] == 45)
-		{
-			min += -1;
-		}
-		while (s[m] >= 48 && s[m] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[m] - '0');
-			m++;
-		}
-		if (isi == 1)
-		{
-			break;
-	        }
-		m++;
-	}
-	ni *= min;
-	return (ni);
+while (s[m])
+{
+if (s[m] == 45)
+{
+min *= -1;
+}
+while (s[m] >= 48 && s[m] <= 57)
+{
+isi = 1;
+ni = (ni * 10) + (s[m] - '0');
+m++;
+}
+if (isi == 1)
+{
+break;
+}
+m++;
+}
+ni *= min;
+return (ni);
 }
