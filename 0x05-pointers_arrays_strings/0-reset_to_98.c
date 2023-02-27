298cb4134b002ff98a7fff51634305be0eb93d6e
alias-ok <okthese@outlook.com>
2023-02-27 12:24:24 +0000

    "0-reset_to_98.c"

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry point that updates the reference of a pointer
+ * @n: Set pointer parameter to store address of n
+ * Return: 98 for success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
