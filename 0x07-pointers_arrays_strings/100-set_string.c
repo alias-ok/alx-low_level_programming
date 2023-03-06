5c1104375b86dae1c4e2d6ef7958d0971f94ee45
alias-ok <okthese@outlook.com>
2023-03-06 18:56:57 +0000

    "8. Double pointer, double fun, a function that sets the value of a pointer to a char"

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - Sets the value of a pointer to char
+ * @s: Double pointer
+ * @to: Pointer to char
+ * Return: ...
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
