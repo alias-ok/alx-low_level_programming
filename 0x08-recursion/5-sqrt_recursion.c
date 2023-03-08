bde003f0319833833b977c916e06318429182a5e
alias-ok <okthese@outlook.com>
2023-03-08 22:15:38 +0000

    "Task 5. A function that returns the natural square root of a number"

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * find_sqrt - Returns the natural square root of a number
+ * @num: number with the natural square root
+ * @root: the root to be tested
+ * Return: r4w
+ */
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
+
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
+	if (root == num / 2)
+		return (-1);
+	return (find_sqrt(num, root + 1));
+}
+/**
+ * _sqrt_recursion - Return the natural square root of a number
+ * @n: The number to return the square root of
+ * Return: if n does not have a natural square root -1
+ *         postive for success
+ */
+int _sqrt_recursion(int n)
+{
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return (find_sqrt(n, root));
+}
