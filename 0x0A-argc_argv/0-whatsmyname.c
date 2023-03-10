fdca2f1a97393acaf21f771118d561779df627cd
alias-ok <okthese@outlook.com>
2023-03-10 13:57:48 +0000

    "a program that prints its name"

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry function that prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
