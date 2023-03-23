67b5fce4b548647f403a964ec652d24b0156f69a
alias-ok <okthese@outlook.com>
2023-03-23 21:28:31 +0000

    "1. To be is to be the value of a variable, a function that prints numbers, followed by a new line"

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - Entry function that prints numbers
+ * @separator: the string to be printed between numbers
+ * @n: the number of integers passed to the function
+ * Return: Always 0 for success
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int index;
+
+	va_start(nums, n);
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(nums, int));
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(nums);
+}
