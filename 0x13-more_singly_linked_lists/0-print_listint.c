3504312cd800d19b69b92daa2ee4bf5b51cf4639
alias-ok <okthese@outlook.com>
2023-04-03 07:57:36 -0700

    "Write a function that prints all the elements of a listint_t list"

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - Entry func that prints the number of nodes
+ * @h: Pointer variable to nodes
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
+	return(nodes);
+}
