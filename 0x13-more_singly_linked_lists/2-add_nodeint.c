6c9e136e40dd17b835a0455ed97e09c02f71665e
alias-ok <okthese@outlook.com>
2023-04-04 02:33:11 -0700

    "Write a function that adds a new node at the beginning of a listint_t list"

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * add_nodeint - function that adds node to beginning
+ * @n: linked list
+ * Return: the address of the new element
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
