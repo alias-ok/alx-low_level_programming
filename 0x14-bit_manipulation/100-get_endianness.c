4da358a7d101ee4e0800188d90ee55e58baa7f04
alias-ok <okthese@outlook.com>
2023-04-09 11:47:41 +0000

    "Write a function that checks the endianness"

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * get_endianness - Entry func that checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int h = 1;
+	char *c = (char *) & h;
+	return (*c);
+}
