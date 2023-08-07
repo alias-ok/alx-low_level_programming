17ea9dcf5d878b0a661a0194ac108f0c8d8063f7
alias-ok <okthese@outlook.com>
2023-08-07 15:21:57 -0700

    Add 0-rrad_textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename
+ * @letters: numbes of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nord, nwer;
+	char *buff;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * (letters));
+	if (!buff)
+		return (0);
+
+	nord = read(fd, buff, letters);
+	nwer = write(STDOUT_FILENO, buff, nord);
+
+	close(fd);
+
+	free(buff);
+
+	return (nwer);
+}
